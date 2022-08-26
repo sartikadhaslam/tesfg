a669b03e536efafd6a2f3db2a26928b891fe87bc
khistynh <khistynh@gmail.com>
2022-08-26 13:11:40 +0700

    add card blog

--- components/Cards/CardBlog.tsx
+++ components/Cards/CardBlog.tsx
@@ -3,79 +3,90 @@ import styles from "../../styles/Home.module.css";
 
 export default function CardBlog() {
   return (
-    <>
-    <div className="container mx-auto px-10 pb-10">
+    <div className="container mx-auto mt-10">
+      {/* header title blog */}
       <div className="flex flex-wrap justify-between">
-        <p className="text-2xl font-bold">Blog Untuk Anda</p>
-        <p className="font-medium text-pink-600">Lihat Semua</p>
+        <p className="text-3xl font-semibold">Blog Untuk Anda</p>
+        <p className="text-xl font-bold text-pink-600">Lihat Semua</p>
       </div>
-      <div className="flex flex-wrap justify-between mt-10">
-        <div className="w-full md:w-5/12">
-          <div className="relative flex flex-col bg-white w-full mb-6 shadow-lg">
-            <Image
-            className="m-0"
-            src="/pct1.png"
-            alt="Landscape picture"
-            width={500}
-            height={200}
-            />
-            <div className="relative p-4">
-              <h1 className="font-bold text-lg text-pink-600">Judul Satu</h1>
-              <div className="float-left flex mt-4">
-                <Image
-                className="rounded-full"
-                src="/pct1.png"
-                alt="Landscape picture"
-                width={30}
-                height={30}
-                />
-                <h1 className="px-2 mr-10 text-sm">Khisty N Hanafi</h1>
-              </div>
-              <div className="float-right flex mt-4">
-              <button
-                className="flloat-right text-sm py-1 px-2 rounded-md bg-pink-600 text-white"
-                type="submit"
-              >
-                Read More
-              </button>
+      {/* end header title blog */}
+
+      {/* item blog */}
+      <div className="flex flex-wrap -mx-6 mt-8">
+        <div className="w-6/12 px-6">
+          <article>
+            <div className="relative flex flex-col bg-gray-200">
+              <Image
+              className="m-0"
+              src="/pct1.png"
+              alt="Landscape picture"
+              width={588}
+              height={278}
+              />
+              <div className="p-4">
+                <h1 className="font-bold text-2xl text-pink-600">Should Designers Code ?</h1>
+                <div className="flex mt-4">
+                  <div className="w-6/12 flex items-center">
+                    <Image
+                    className="rounded-full"
+                    src="/pct1.png"
+                    alt="Landscape picture"
+                    width={40}
+                    height={40}
+                    />
+                    <h1 className="px-2 mr-10 text-xl font-semibold">Nadine Chandra</h1>
+                  </div>
+                  <div className="w-6/12 text-right">
+                    <button
+                      className="text-right text-sm py-2 px-4 rounded-lg bg-pink-600 text-white"
+                      type="submit"
+                      >
+                      Read More
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
+          </article>
         </div>
-        <div className="w-full md:w-5/12">
-          <div className="relative flex flex-col min-w-0 bg-white w-full mb-6 shadow-lg">
-            <Image
-            className="m-0"
-            src="/pct1.png"
-            alt="Landscape picture"
-            width={500}
-            height={200}
-            />
-            <div className="relative p-4">
-              <h1 className="font-bold text-lg text-pink-600">Judul Satu</h1>
-              <div className="float-left flex mt-4">
-                <Image
-                className="rounded-full"
-                src="/pct1.png"
-                alt="Landscape picture"
-                width={30}
-                height={30}
-                />
-                <h1 className="px-2 mr-10 text-sm">Khisty N Hanafi</h1>
-              </div>
-              <div className="float-right flex mt-4">
-              <button
-                className="flloat-right text-sm py-1 px-2 rounded-md bg-pink-600 text-white"
-                type="submit"
-              >
-                Read More
-              </button>
+        <div className="w-6/12 px-6">
+          <article>
+            <div className="relative flex flex-col bg-gray-200">
+              <Image
+              className="m-0"
+              src="/pct2.png"
+              alt="Landscape picture"
+              width={588}
+              height={278}
+              />
+              <div className="p-4">
+                <h1 className="font-bold text-2xl text-pink-600">Tips Perempuan bisa PD Mengoding</h1>
+                <div className="flex mt-4">
+                  <div className="w-6/12 flex items-center">
+                    <Image
+                    className="rounded-full"
+                    src="/pct1.png"
+                    alt="Landscape picture"
+                    width={40}
+                    height={40}
+                    />
+                    <h1 className="px-2 mr-10 text-xl font-semibold">Nadine Chandra</h1>
+                  </div>
+                  <div className="w-6/12 text-right">
+                    <button
+                      className="text-right text-sm py-2 px-4 rounded-lg bg-pink-600 text-white"
+                      type="submit"
+                      >
+                      Read More
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
+          </article>
         </div>
       </div>
+      {/* end item blog */}
     </div>
-    </>
   );
 }
