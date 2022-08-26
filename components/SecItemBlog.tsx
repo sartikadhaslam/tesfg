c09ebe1b2ed55a2f486eea8147db19c4970812aa
khistynh <khistynh@gmail.com>
2022-08-26 13:10:15 +0700

    add section item blog

---
+++ components/SecItemBlog.tsx
@@ -0,0 +1,11 @@
+import CardBlog from "./Cards/CardBlog";
+
+export default function SecItemBlog() {
+  return (
+    <section className="pb-20">
+          <CardBlog />
+          <CardBlog />
+          <CardBlog />
+    </section>
+  )
+}
