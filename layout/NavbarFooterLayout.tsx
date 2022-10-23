b35b5f72998499f8926107738348fcdb1ec326f0
khistynh <khistynh@gmail.com>
2022-10-23 12:11:43 +0700

    ubah

--- layout/NavbarFooterLayout.tsx
+++ layout/NavbarFooterLayout.tsx
@@ -29,7 +29,7 @@ export default function NavbarFooterLayout(props: NavbarFooterLayoutProps) {
                 className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                 aria-controls="mobile-menu-2"
                 aria-expanded="false"
-              >
+                >
                 <span className="sr-only">Open main menu</span>
                 <svg
                   className="w-6 h-6"
