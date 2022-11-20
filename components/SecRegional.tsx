293044dbb7731d64a146da2ca1904e947836b5ae
Tikaaa <dwisartika346@gmail.com>
2022-11-20 11:59:09 +0700

    Section Regional

--- components/SecRegional.tsx
+++ components/SecRegional.tsx
@@ -5,7 +5,7 @@ export default function SecRegional() {
     <section className="mt-10 md:mt-20 pb-40 relative">
       <div className="container mx-auto justify-center text-center flex flex-wrap">
         <div className="w-full">
-          <h2 className="font-bold text-4xl">ABOUT US</h2>
+          <h2 className="font-bold text-4xl">REGIONAL</h2>
           <div className="mt-10 flex justify-center">
             <img className="h-full w-full lg:w-full" src="/Regional.png" />
           </div>
