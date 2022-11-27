edb3c0e1bf7aff844a1c560a4584324175b3ecc2
Tikaaa <dwisartika346@gmail.com>
2022-11-27 14:13:59 +0700

    SecPartners

--- components/SecRegional.tsx
+++ components/SecRegional.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function SecRegional() {
   return (
-    <section className="mt-10 md:mt-20 pb-40 relative">
+    <section className="mt-10 relative">
       <div className="container mx-auto justify-center text-center flex flex-wrap">
         <div className="w-full">
           <h2 className="font-bold text-4xl">REGIONAL</h2>
